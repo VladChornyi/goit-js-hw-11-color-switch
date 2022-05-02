c397ecceb64eec6c197866701c0e45f715cb7e20
Chornyi Vlad <4ornii.vlad@gmail.com>
2022-05-02 12:39:26 +0300

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import './sass/main.scss';
 
-const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+const colors: string[] = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
-const randomIntegerFromInterval = (min, max) => {
+type GetRandomNumber = (min: number, max: number) => number;
+
+const randomIntegerFromInterval: GetRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 const bodyNode = document.querySelector('body');
 
-const startBtnNode = document.querySelector('button[data-action="start"]');
+const startBtnNode: HTMLButtonElement = document.querySelector('button[data-action="start"]');
 
-const stopBtnNode = document.querySelector('button[data-action="stop"]');
+const stopBtnNode: HTMLButtonElement = document.querySelector('button[data-action="stop"]');
 
-const itervalLength = 1000;
+const itervalLength: number = 1000;
 
 let isOnChange = false;
 
-let changeBcgByInterval;
+let changeBcgByInterval: NodeJS.Timer;
 
 startBtnNode.addEventListener('click', e => {
   if (!isOnChange)
